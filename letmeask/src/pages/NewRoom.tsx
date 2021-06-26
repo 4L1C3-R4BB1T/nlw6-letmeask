fd79263ee549c126708688e26743568fb413a11a
Livia <livinha.guimaraes.013@gmail.com>
2021-06-25 23:26:23 -0300

    add routes

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
@@ -27,7 +29,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala já existente? <a href="#">clique aqui</a>
+                        Quer entrar em uma sala já existente? <Link to="/">clique aqui</Link>
                     </p>
                 </div>
             </main>
