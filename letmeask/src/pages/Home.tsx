632020e1f3bca9a8f0d59d747fbefd89fa57724a
Livia <livinha.guimaraes.013@gmail.com>
2021-07-12 16:01:44 -0300

    add create question functionality

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -36,7 +36,7 @@ export function Home() {
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
         if (!roomRef.exists()) {
-            alert('Room does not exists.');
+            alert('A sala informada não existe.');
             return;
         }
 
